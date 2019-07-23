75b14741d9a2953ab06c1bca161c4cf9fcc129a9
Donna Kuang <dolaku@gmail.com>
2019-07-23 13:51:36 -0400

    Begin front-end
    -some components
    -work on login/registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar"
+// import HabitsList from "./components/HabitsList"
+// import EditHabit from "./components/EditHabit"
+// import CreateHabit from "./components/CreateHabit"
+// import CreateUser from "./components/CreateUser"
 
 function App() {
   return (
-    <div className="App">
-      <h1>This works!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={HabitsList} />
+      <Route path="/edit/:id" component={EditHabit} />
+      <Route path="/create" component={CreateHabit} />
+      <Route path="/user" component={CreateUser} />
+
+    </Router>
   );
 }
 
