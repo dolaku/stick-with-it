8060cbde36eb55906705f41d7aec973dc0bf4eb8
dolaku <dolaku@gmail.com>
2019-07-24 21:52:32 -0400

    fixed email link

--- client/src/components/layout/Footer/index.js
+++ client/src/components/layout/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => (
     <footer className="pt-3 bg-light">
         <div className="d-flex flex-column justify-content-center align-items-center">
             <h5><a href="https://dolaku.github.io/" target="_blank" rel="noopener noreferrer">Donna Kuang</a></h5>
-            <Link className="link-email" to="mailto:dolaku@gmail.com">dolaku@gmail.com</Link>
+            <a className="link-email" href="mailto:dolaku@gmail.com">dolaku@gmail.com</a>
             <Link className="display-4 link-email" to="https://github.com/dolaku" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></Link>
             <p className="small">&copy; Copyright 2019. All Rights Reserved.</p>
         </div>
