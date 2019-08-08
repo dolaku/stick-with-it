1b9ac03f85bd5e75c274e3c8d81343ee8d60ffdf
dolaku <dolaku@gmail.com>
2019-08-07 21:02:22 -0400

    testing heroku deploy

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// server static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
