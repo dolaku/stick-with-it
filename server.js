9f4d1682a67ea423bbac04fa40a91da1aea39449
dolaku <dolaku@gmail.com>
2019-08-08 18:27:24 -0400

    Updated server.js, testing heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require('express').Router();
 
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // server static assets
